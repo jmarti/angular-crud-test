c99eb3117693a60b4546956b8919065a42fdf4b3
Jordi Martí Domínguez <jmarti@MacBook-Pro-de-Jordi.local>
2020-09-10 11:35:45 -0500

    added all functionality except filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Service, Category } from './app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-crud-test';
+export class AppComponent implements OnInit {
+  services: Service[];
+  categories: Category[];
+  serviceForm: Service = {
+    id: null,
+    title: null,
+    description: null,
+    category: null
+  };
+
+  constructor(
+    private appService: AppService
+  ) { }
+
+  ngOnInit() {
+    this.services = this.appService.getItems();
+    this.categories = this.appService.getCategories();
+  }
+
+  onSelectToEditService(service: Service) {
+    this.serviceForm = Object.assign({}, service);
+  }
+
+  onDeleteService(serviceId: number) {
+    this.services = this.appService.deleteService(serviceId);
+
+    if (serviceId === this.serviceForm.id) {
+      this.resetForm();
+    }
+  }
+
+  onSaveForm() {
+    if (!this.serviceForm.title ||
+        !this.serviceForm.description ||
+        !this.serviceForm.category) {
+      return;
+    }
+    
+    if (this.serviceForm.id) {
+      this.appService.editService(this.serviceForm);
+    } else {
+      this.appService.addService(this.serviceForm);
+    }
+
+    this.resetForm();
+  }
+
+  onResetForm() {
+    this.resetForm();
+  }
+
+  private resetForm() {
+    this.serviceForm = {
+      title: null,
+      description: null,
+      category: null
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
